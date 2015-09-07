74ae3532eedd458143db0d66163657d6cdc032b5
Ariel Fuentes <ariel.fuentesdi@usach.cl>
2015-09-07 17:00:28 -0300

    Update buffer+intersect.sql

--- buffer+intersect.sql
+++ buffer+intersect.sql
@@ -2,3 +2,9 @@ SELECT ST_Buffer(eod_2012.geom, 50) as buffer, eod_2012.gid as gideod, zonas_ts.
 from zonas_ts, eod_2012 
 where eod_2012.id = '103'
 and ST_Intersects(zonas_ts.geom, eod_2012.geom);
+---------------------------------------------------------------------------------------------------------------
+SELECT ST_Buffer(eod_2012.geom, 50) as buffer, eod_2012.gid as gideod, servicios_ago22_2015.gid as gidser, 
+ST_Transform(servicios_ago22_2015.geom, 32719) as geomser 
+from servicios_ago22_2015, eod_2012 
+where servicios_ago22_2015.route_name = 'B01I'
+and ST_Intersects(ST_Transform(servicios_ago22_2015.geom, 32719), eod_2012.geom);
